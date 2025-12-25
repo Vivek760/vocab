a2daae9ba4c63cc4b40fb550212e72a5c77a5839
Vivek760 <vivekmenon760@gmail.com>
2025-12-25 16:06:15 +0530

    sidebar bug

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
+
 st.set_page_config(
     page_title="Study Dashboard",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
+st.sidebar.write("Navigation")
 
 # st.title("📚 Study Dashboard")
 st.write("Use the sidebar to navigate between Vocabulary and Quant.")
+
